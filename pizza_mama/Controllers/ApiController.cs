e2e66cee5e3e02cec3f9822bb037d9805a042ae7
Joris <contact@jorisreynes.fr>
2022-06-15 00:03:23 +0200

    Ajoutez des fichiers projet.

---
+++ pizza_mama/Controllers/ApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pizza_mama.Data;
+using pizza_mama.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace pizza_mama.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiController : Controller
+    {
+
+        DataContext datacontext;
+        public ApiController(DataContext dataContext)
+        {
+            this.datacontext = dataContext;
+        }
+
+
+        [HttpGet]
+        [Route("GetPizzas")]
+        public IActionResult GetPizzas()
+        {
+            //var pizza = new Pizza() { nom = "pizza_test", prix = 8, vegetarienne = false, ingredients = "tomate, oignons, oeuf" };
+
+
+            var pizzas = datacontext.Pizzas.ToList();
+
+            return Json(pizzas);
+        }
+
+    }
+}
---
+++ pizza_mama/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace pizza_mama.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public bool DisplayInvalidAccountMessage = false;
+        public bool IsDevelopmentMode = false;
+
+        IConfiguration configuration;
+
+        public IndexModel(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            this.configuration = configuration;
+
+            if (env.IsDevelopment())
+            {
+                IsDevelopmentMode = true;
+            }
+        } 
+
+        public IActionResult OnGet()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Redirect("Admin/Pizzas");
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string username, string password, string ReturnUrl)
+        {
+            var authSection = configuration.GetSection("Auth");
+
+            string adminLogin = authSection["adminLogin"];
+            string adminPassword = authSection["adminPassword"];
+
+            if ((username == adminLogin) && (password == adminPassword))
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, username)
+                };
+                var claimsIdentity = new ClaimsIdentity(claims, "Login");
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new
+               ClaimsPrincipal(claimsIdentity));
+                return Redirect(ReturnUrl == null ? "/Admin/Pizzas" : ReturnUrl);
+            }
+            DisplayInvalidAccountMessage = true;    
+            return Page();
+        }
+
+        public async Task<IActionResult> OnGetLogout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/Admin");
+        }
+
+    }
+}
